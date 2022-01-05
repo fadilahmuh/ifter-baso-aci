543b52ab76bd1f7371c76b004a3dffce73fa9727
fadilahmuh <67127698+fadilahmuh@users.noreply.github.com>
2022-01-05 17:12:41 +0700

    Log

---
+++ app/Models/Log.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Log extends Model
+{
+    protected $fillable = [
+        'keterangan',
+        'type',
+        'transactions_id',
+        'users_id'
+    ];
+
+    public function users(){
+    	return $this->belongsTo(User::class);
+    }
+
+    public function transactions(){
+    	return $this->belongsTo(Transaction::class);
+    }
+}
---
+++ database/migrations/2022_01_05_095836_create_logs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger("users_id")->index();
+            $table->unsignedInteger("transactions_id")->index();
+            $table->string('keterangan')->nullable(true);
+            $table->char('type')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
