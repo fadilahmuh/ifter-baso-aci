8b5c9661e97ecfd8bcbebefacefd293ce23e895e
Andika Fauzi Hadiana <doneka54@gmail.com>
2022-01-10 19:58:05 +0700

    logo-login~register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -33,7 +33,7 @@
         <div class="row">
           <div class="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
             <div class="login-brand">
-              <img src="assets-landing/img/BACKGROUND.png" alt="logo" width="100">
+              <img src="assets-landing/img/logoo.png" alt="logo" width="100">
             </div>
 
             <div class="card card-primary">
